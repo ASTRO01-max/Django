9f7156c590e47bbe6fd8d1ebddf0249688b3ad19
ASTRO01-max <mfulsefer@gmail.com>
2025-05-15 10:58:14 +0500

    Django table

--- table/views.py
+++ table/views.py
@@ -10,9 +10,9 @@ def week(request, week_name):
 def home(request):
     html = """
     <p><a href=' http://127.0.0.1:8000/table/Dushanba'>Monday<p/>
-    <p><a href=' http://127.0.0.1:8000/table/Seshanba'>Thuestday<p/>
+    <p><a href=' http://127.0.0.1:8000/table/Seshanba'>Tuesday<p/>
     <p><a href=' http://127.0.0.1:8000/table/Chorshanba'>Wednesday<p/>
-    <p><a href=' http://127.0.0.1:8000/table/Payshanba'>Thostday<p/>
+    <p><a href=' http://127.0.0.1:8000/table/Payshanba'>Thursday<p/>
     <p><a href=' http://127.0.0.1:8000/table/Juma'>Friday<p/>
     <p><a href=' http://127.0.0.1:8000/table/Shanba'>Saturday<p/>
     <p><a href=' http://127.0.0.1:8000/table/Yakshanba'>Sunday<p/>
